9343d36b8449c594f6dbb2616dad4135e093a1da
Rentoo <dreamYiZ@outlook.com>
2023-09-04 05:29:53 +0800

    fn arg

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -2,10 +2,10 @@ fn main(){
 
     println!("main function");
 
-    another_function();
+    another_function(7);
 }
 
 
-fn another_function(){
-    println!("another function");
+fn another_function(x: i32){
+    println!("the value of x is {x}");
 }
