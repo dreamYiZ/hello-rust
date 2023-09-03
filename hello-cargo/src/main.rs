f87c5e191e0506ac33759556b4963624256cb348
Rentoo <dreamYiZ@outlook.com>
2023-09-04 06:38:49 +0800

    ref 1

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,17 +1,14 @@
-fn main() {
+fn main(){
+
     let s1 = String::from("hello");
 
-    let (s2,l) = calculate_length(s1);
+    let l = calculate_len(&s1);
 
-    // println!("{s1}");
+    println!("{s1} {l}");
 
-    println!("{s2} {l}");
-}
-
-fn calculate_length(s: String) -> (String, usize) {
-    let s_len = s.len();
-
-    (s, s_len)
 }
 
 
+fn calculate_len(s: &String)->usize{
+    s.len()
+}
