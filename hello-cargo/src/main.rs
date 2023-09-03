7a9c5721d2a92f73daf2595a26dba952f40ccf7f
Rentoo <dreamYiZ@outlook.com>
2023-09-04 05:24:28 +0800

    array

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,11 +1,33 @@
-fn main() {
-    let x = 5;
-    let x = x + 1;
+use std::io;
 
-    {
-        let x = x * 2;
-        println!("The x in scope is {}",x);
+fn main() {
+    let mut arr = [1, 2, 3, 4, 5];
+
+    // for mut item in arr.into_iter() {
+    //     item = item * 2;
+    // }
+    let mut i: usize = 0;
+
+    while i < arr.len() {
+        arr[i] = arr[i] * 2;
+        i += 1;
     }
 
-    println!("The value of x is {}", x);
+    // for (index, item) in arr.iter().enumerate() {
+    //     arr[index] = item * 2;
+    // }
+
+    println!("Please enter a index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index.trim().parse().expect("Index Enter was not a number");
+
+    let element = arr[index];
+
+    println!("The value of element at index {index} is {element}");
 }
