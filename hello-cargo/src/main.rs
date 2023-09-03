29b5e674fa08324c861f3d1e605711054db95d3b
Rentoo <dreamYiZ@outlook.com>
2023-09-04 06:15:22 +0800

    ownership

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,8 +1,21 @@
 fn main(){
 
+  let x = String::from("hello");
 
-    let x = 5;
-    let y = x;
+  take_ownership(x);
 
-    println!("x = {}, y = {}.", x, y);
+  let x = 5;
+
+  make_copy(x);
+  
+}
+
+
+fn take_ownership(some_string: String){
+
+    println!("{}", some_string);
+}
+
+fn make_copy(some_integer: i32){
+    println!("{}", some_integer);
 }
