eb859ab1dad624e4619697e8396aba262734807e
Rentoo <dreamYiZ@outlook.com>
2023-09-04 05:06:27 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,46 +1,11 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-    println!("Guess the number!");
+    let x = 5;
+    let x = x + 1;
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {}", secret_number);
-
-    let mut guess = String::new();
-
-    loop {
-        println!("Please input your guess.");
-
-        guess.clear();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Smaller than"),
-            Ordering::Greater=> println!("Greater than"),
-            Ordering::Equal =>{
-                println!("You Win!");
-                break;
-            }
-        }
-
-        // if secret_number == guess {
-        //     println!("You win!");
-        //     break;
-        // } else {
-        //     println!("Sorry, try again.");
-        // }
+    {
+        let x = x * 2;
+        println!("The x in scope is {}",x);
     }
+
+    println!("The value of x is {}", x);
 }
