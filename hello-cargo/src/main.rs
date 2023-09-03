9757bbce90851a7ee1f0fe5e0686234b15f65e6e
Rentoo <dreamYiZ@outlook.com>
2023-09-04 06:51:21 +0800

    borrowed 3

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -3,14 +3,10 @@ fn main(){
 
     let mut s = String::from("hello");
 
-    {
-        let r1 = &mut s;
+    let r2 = &s;
+    let r3 = &s;
 
+    let r4 = &mut s;
 
-    }
-
-
-    let r2  = &mut s;
-
-    println!("{r2}");
+    println!("{r2}{r3}{r4}")
 }
