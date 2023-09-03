7dfd1185e8dcc46a8584dab323745a24e7d9f18a
Rentoo <dreamYiZ@outlook.com>
2023-09-04 05:28:55 +0800

    fn

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,33 +1,11 @@
-use std::io;
+fn main(){
 
-fn main() {
-    let mut arr = [1, 2, 3, 4, 5];
+    println!("main function");
 
-    // for mut item in arr.into_iter() {
-    //     item = item * 2;
-    // }
-    let mut i: usize = 0;
+    another_function();
+}
 
-    while i < arr.len() {
-        arr[i] = arr[i] * 2;
-        i += 1;
-    }
 
-    // for (index, item) in arr.iter().enumerate() {
-    //     arr[index] = item * 2;
-    // }
-
-    println!("Please enter a index.");
-
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index.trim().parse().expect("Index Enter was not a number");
-
-    let element = arr[index];
-
-    println!("The value of element at index {index} is {element}");
+fn another_function(){
+    println!("another function");
 }
