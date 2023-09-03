cff9685f4b828838cc70efce241d0dfaf058dd4b
Rentoo <dreamYiZ@outlook.com>
2023-09-04 06:11:05 +0800

    x,y

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,20 +1,8 @@
 fn main(){
 
 
-    let mut str = String::from("Hello");
+    let x = 5;
+    let y = x;
 
-    str.push_str(", World!");
-
-
-    println!("{str}");
-
-    let s1 = "hello";
-    let s2 = "world!";
-
-    println!("{s1} {s2}");
-
-    let s3 = "hello";
-    let s4 = s3.clone();
-
-    println!("s3 == s4? {} {}", s3 == s4, ".");
+    println!("x = {}, y = {}.", x, y);
 }
