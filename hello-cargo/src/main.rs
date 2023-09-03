834e5363c053924f63691bdce76ead3d6feb3d9b
Rentoo <dreamYiZ@outlook.com>
2023-09-03 23:37:41 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,17 +1,37 @@
 use std::io;
+use rand::Rng;
+
 
 fn main() {
-    println!("Hello, world Cargo!");
-    println!("Guess the number");
+    println!("Guess the number!");
 
-    println!("Please enter your number");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {}", secret_number);
 
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
+        guess.clear();
 
-    println!("Your guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        if secret_number == guess {
+            println!("You win!");
+            break;
+        } else {
+            println!("Sorry, try again.");
+        }
+    }
 }
