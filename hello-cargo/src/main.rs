8ed1df43e93ad6d41243e42df29267413089431d
Rentoo <dreamYiZ@outlook.com>
2023-09-04 15:11:12 +0800

    loop

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -4,7 +4,7 @@ fn main(){
     let my_string = String::from("hello world");
 
 
-    for (index, item ) in my_string.iter() {
+    for (index, item ) in my_string.chars().enumerate() {
 
         println!("index {}, item {}", index, item);
     }
