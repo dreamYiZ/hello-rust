6dfe2993925bf5745f0f9a3a073744421f1d6584
Rentoo <dreamYiZ@outlook.com>
2023-09-04 15:08:31 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,19 +1,12 @@
-fn main() {
-    let s = String::from("Hello World!");
+fn main(){
 
-    let word = first_word(&s);
 
-    println!("{}", word);
-}
+    let my_string = String::from("hello world");
 
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
+    for (index, item ) in my_string.iter() {
+
+        println!("index {}, item {}", index, item);
     }
 
-    &s[..]
 }
