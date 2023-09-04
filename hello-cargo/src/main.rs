0b86109c4e2424c8f0eaaf3c8eafb72c5c70cd1e
Rentoo <dreamYiZ@outlook.com>
2023-09-05 04:35:25 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,30 +1,12 @@
-fn main(){
+fn main() {
+    let mut s = String::from("foo");
 
+    s.push_str(" bar");
 
-    let mut v = Vec::new();
+    let s2 = "!";
 
+    s.push_str(&s2);
+    s.push_str(s2);
 
-    v.push(5);
-    v.push(5);
-    v.push(5);
-    v.push(5);
-
-    println!("{:?}", v);
-
-
-
-    let v2 = vec![1,2,3,4,5,6];
-
-    let third : &i32 = &v[2];
-
-
-    println!("third is {}", third);
-
-    let third2 :Option<&i32> = v.get(2);
-
-
-    match third2 {
-        Some(third2) => println!("the third is {}", third2),
-        None =>  println!("there is no third2")
-    }
+    println!("{}", s);
 }
