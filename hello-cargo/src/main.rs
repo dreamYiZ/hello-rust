0d30c9a0cacf219bc2c6301393e8b2bd45989407
Rentoo <dreamYiZ@outlook.com>
2023-09-04 15:35:26 +0800

    struct

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,12 +1,40 @@
-fn main(){
+
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user = User {
+        active: true,
+        username: String::from("rentoo"),
+        email: String::from("rentoo@ren.com"),
+        sign_in_count: 1,
+    };
+
+    let user2 = build_user(String::from("rentoo2"),
+                                    String::from("rentoo2@rentoo.com"));
 
 
-    let my_string = String::from("hello world");
+    println!("{:?}", user2);
+// 
+    let user3 = User{
+        email: String::from("rentoo3@rentoo.com"),
+        ..user2
+    };
 
+    println!("{:?}", user);
+    println!("{:?}", user3);
+}
 
-    for (index, item ) in my_string.chars().enumerate() {
-
-        println!("index {}, item {}", index, item);
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
     }
-
 }
