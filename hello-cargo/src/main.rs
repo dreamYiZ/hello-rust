66b9fd313ea16e1a837b0497d275709edd1e41fe
Rentoo <dreamYiZ@outlook.com>
2023-09-04 19:28:03 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,25 +1,48 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
+#[derive(Debug)]
+enum UsState {
+
+    Alabama,
+    Alaska,
 }
 
 
-impl Rectangle{
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-}
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
 
 
 fn main(){
 
-    let rect1 = Rectangle{
-        width: 30,
-        height: 50,
-    };
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?}", six);
+    println!("{:?}", none);
+}
 
 
+fn value_in_cents(coin: Coin)->u8{
+    match coin{
+        Coin::Penny => 1,
+        Coin::Nickel =>5,
+        Coin::Dime => 10,
+        Coin::Quarter(state)=>{
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
 
-    println!("the area of the rectangle is {}", rect1.area());
+
+fn plus_one (x: Option<i32>)-> Option<i32>{
+    match x {
+        None=>None,
+        Some(i)=> Some(i+1)
+    }
+
 }
