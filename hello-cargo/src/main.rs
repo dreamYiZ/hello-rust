bcf4bfc1f25bcc366efc81449a663853afdf5d02
Rentoo <dreamYiZ@outlook.com>
2023-09-04 16:19:11 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,25 +1,17 @@
-
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
-    let width = 50;
-    let height = 30;
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
-    let rect1 = (50,32);
-
-    let area1 = area(width, height);
-
-    println!("area is {}", area1);
-
-
-    println!("area2 is {}", get_area_2(rect1));
+    println!("The area of the rectangle is {}", area(&rect1));
 }
 
-fn area(w: i32, h: i32) -> i32 {
-    w * h
-}
-
-
-fn get_area_2 (dimensions: (i32, i32))->i32{
-
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
