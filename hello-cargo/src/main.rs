5060f5b3f6e2869769499c939d60b13174a688cf
Rentoo <dreamYiZ@outlook.com>
2023-09-05 04:47:07 +0800

    hashmap2

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,12 +1,16 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
 
-    scores.insert(String::from("blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let text = "hello world wonderful world";
 
-    for (key, value) in &scores {
-        println!("{key}: {value}");
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+
+        *count += 1;
     }
+
+    println!("{:?}", map);
 }
