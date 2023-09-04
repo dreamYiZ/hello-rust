a46aa7a788c99c5b65b926d4178ed96f2f143cc8
Rentoo <dreamYiZ@outlook.com>
2023-09-04 19:35:14 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,48 +1,15 @@
-#[derive(Debug)]
-enum UsState {
+fn main() {
+    let dice_roll = 9;
 
-    Alabama,
-    Alaska,
-}
-
-
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-
-fn main(){
-
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-
-    println!("{:?}", six);
-    println!("{:?}", none);
-}
-
-
-fn value_in_cents(coin: Coin)->u8{
-    match coin{
-        Coin::Penny => 1,
-        Coin::Nickel =>5,
-        Coin::Dime => 10,
-        Coin::Quarter(state)=>{
-            println!("State quarter from {:?}", state);
-            25
-        }
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),
     }
 }
 
+fn add_fancy_hat() {}
 
-fn plus_one (x: Option<i32>)-> Option<i32>{
-    match x {
-        None=>None,
-        Some(i)=> Some(i+1)
-    }
+fn remove_fancy_hat() {}
 
-}
+fn reroll() {}
