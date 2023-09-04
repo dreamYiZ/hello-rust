3d03f307f2ec9f91da012b3a2e3592751b697a63
Rentoo <dreamYiZ@outlook.com>
2023-09-04 16:22:21 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,17 +1,15 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    println!("The area of the rectangle is {}", area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    dbg!(&rect1);
 }
