07ea4b9eb0a89bfc689760b3028de89ac750bb41
Rentoo <dreamYiZ@outlook.com>
2023-09-06 15:36:12 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,6 +1,27 @@
-fn main (){
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
 
-    let b = Box::new (5);
 
-    println!("b = {}", b);
+use crate::List::{Cons, Nil};
+
+use std::rc::Rc;
+
+fn main() {
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+
+    // let b = Cons (3, Box::new(a));
+    // let c = Cons(4, Box::new (a));
+    let b:List = Cons(3, Rc::clone(&a));
+
+    println!("count is {}", Rc::strong_count(&a));
+    {
+        let c:List = Cons(5, Rc::clone(&a));
+        println!("count is {}", Rc::strong_count(&a));
+
+    }
+    println!("count is {}", Rc::strong_count(&a));
+
 }
