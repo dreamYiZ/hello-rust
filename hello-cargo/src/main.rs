1865b7765b97306dd94c5d1c6f56b758d72e63f4
Rentoo <dreamYiZ@outlook.com>
2023-09-06 16:27:42 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,18 +1,10 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("spawn: {}", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1, 2, 3];
+    let handel = thread::spawn(move || {
+        println!("here's a vector: {:?}", v);
     });
 
-    for i in 1..5 {
-        println!("main thread: {}", i);
-        thread::sleep(Duration::from_millis(1));
-    }
-
-    handle.join().unwrap();
+    handel.join().unwrap();
 }
