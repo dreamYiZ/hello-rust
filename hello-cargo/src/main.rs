86d6687c9d3348e85b54319af8c2b6ad56d4b8e1
Rentoo <dreamYiZ@outlook.com>
2023-09-06 15:53:13 +0800

    RefCell

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,27 +1,24 @@
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons (Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
-
 use crate::List::{Cons, Nil};
-
+use std::cell::RefCell;
 use std::rc::Rc;
 
-fn main() {
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+fn main(){
+    let value = Rc::new (RefCell::new(5));
 
-    // let b = Cons (3, Box::new(a));
-    // let c = Cons(4, Box::new (a));
-    let b:List = Cons(3, Rc::clone(&a));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new (Nil)));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone (&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone (&a));
 
-    println!("count is {}", Rc::strong_count(&a));
-    {
-        let c:List = Cons(5, Rc::clone(&a));
-        println!("count is {}", Rc::strong_count(&a));
+    *value.borrow_mut() += 10;
 
-    }
-    println!("count is {}", Rc::strong_count(&a));
-
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
