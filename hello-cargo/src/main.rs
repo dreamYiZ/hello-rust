3d5b36389f82b91f6da570ea5d5cf882401cc93a
Rentoo <dreamYiZ@outlook.com>
2023-09-06 14:58:02 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,18 +1,6 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+fn main (){
 
-fn main(){
+    let b = Box::new (5);
 
-    let mut list = [
-        Rectangle {width :10 ,height:6},
-        Rectangle { width: 3, height: 2},
-        Rectangle {width :7, height:5},
-    ];
-
-    list.sort_by_key(|r| r.width);
-
-    println!("{:#?}" ,list);
+    println!("b = {}", b);
 }
