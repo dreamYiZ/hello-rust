8057c63da62e19fe8f69dbbcac5ecdc5013ce627
Rentoo <dreamYiZ@outlook.com>
2023-09-07 23:24:42 +0800

    -

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,10 +1,26 @@
-use std::thread;
-
 fn main() {
-    let v = vec![1, 2, 3];
-    let handel = thread::spawn(move || {
-        println!("here's a vector: {:?}", v);
-    });
+    let mut count = 0u32;
 
-    handel.join().unwrap();
+    println!("Let's count until infinity!");
+
+    // Infinite loop
+    loop {
+        count += 1;
+
+        if count %2 == 0 {
+            println!("odd {}", count);
+
+            // Skip the rest of this iteration
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 15 {
+            println!("OK, that's enough");
+
+            // Exit this loop
+            break;
+        }
+    }
 }
